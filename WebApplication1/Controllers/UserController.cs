acde9cbd0b6d114b2f5371da4dcbdf14a60fd509
Đan Trần <you@example.com>
2022-09-08 11:13:26 +0700

    first commit

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        static List<User> users = new List<User>();
+        [HttpGet("GetUsers")]
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            //users.Add(new User("tk", "khang", 21, "32dsss"));
+            return users;
+        }
+        [HttpGet("GetUserName")]
+        public User GetUserName(string UserName)
+        {
+            foreach (var user in users)
+            {
+                if (user.UserName == UserName)
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
+        [HttpPost("InputUser")]
+        public IActionResult InputUser(User newUser)
+        {
+            if(newUser.UserName != null)
+            {
+                foreach (var user in users)
+                {
+                    if (newUser.UserName == user.UserName)
+                    {
+                        return BadRequest("Already have this user");
+                    }
+                }
+                users.Add(newUser);
+                return Ok("Add success");
+            }
+            return BadRequest("Input is empty");
+        }
+
+        [HttpPut("UpdateUser")]
+        public IActionResult UpdateUser(string userName, User updateuser)
+        {
+            foreach (var user in users)
+            {
+                if (userName == user.UserName)
+                {
+                    user.Age = updateuser.Age;
+                    user.Address = updateuser.Address;
+                    return Ok("Success");
+                }
+            }
+            return BadRequest("Cannot find user");
+        }
+        [HttpDelete("DeleteUser")]
+        public IActionResult DeleteUser(string userName)
+        {
+            foreach(var user in users)
+            {
+                if(userName == user.UserName)
+                {
+                    users.Remove(user);
+                    return Ok("Complete Delete!");
+                }    
+            }
+            return BadRequest("Cannot find user!");
+        }
+    }
+}
---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.Models
+{
+    public class User
+    {
+        public User(string userName, string name, int age, string address)
+        {
+            UserName = userName;
+            Name = name;
+            Age = age;
+            Address = address;
+        }
+
+        public string UserName { get; set; } 
+        public string Name{ get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
+}
